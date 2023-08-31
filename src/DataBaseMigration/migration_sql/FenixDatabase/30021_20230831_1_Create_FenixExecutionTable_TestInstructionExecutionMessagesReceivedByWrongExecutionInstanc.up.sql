d26335ca582c4512697ec3a470c5d8db2ef84c31
jlamber <jonas.lambert@drakon.se>
2023-08-31 18:48:21 +0200

    Added SQL for: 'create table "FenixExecution"."TestInstructionExecutionMessagesReceivedByWrongExecutionInstanc"'

---
+++ src/DataBaseMigration/migration_sql/FenixDatabase/30021_20230831_1_Create_FenixExecutionTable_TestInstructionExecutionMessagesReceivedByWrongExecutionInstanc.up.sql
@@ -0,0 +1,23 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+BEGIN;
+
+create table "FenixExecution"."TestInstructionExecutionMessagesReceivedByWrongExecutionInstanc"
+(
+    "ApplicationExecutionRuntimeUuid" uuid,
+    "TestInstructionExecutionUuid"    uuid      not null,
+    "TestInstructionExecutionVersion" integer   not null,
+    "TimeStamp"                       timestamp not null,
+    "MessageType"                     integer   not null,
+    "MessageAsJsonb"                  jsonb     not null
+);
+
+comment on table "FenixExecution"."TestInstructionExecutionMessagesReceivedByWrongExecutionInstanc" is 'Temporary holds TestInstructionExecution-data that is handled by other Execution-instance';
+
+alter table "FenixExecution"."TestInstructionExecutionMessagesReceivedByWrongExecutionInstanc"
+    owner to postgres;
+
+
+
+COMMIT;
+
