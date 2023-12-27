f9e9ac08a93929f5facc2bb8e5acd25f400cc659
jlambert68 <jonas.lambert@drakon.se>
2023-12-27 16:31:24 +0100

    Added column "callingworkerserviceaccountname" in Domains

--- DataBaseMigration/migration_sql/FenixDatabase/00002_20230428_1_Create_Table_domains.up.sql
+++ DataBaseMigration/migration_sql/FenixDatabase/00002_20230428_1_Create_Table_domains.up.sql
@@ -5,22 +5,24 @@ BEGIN;
 -- Create the Domains-table
 CREATE TABLE IF NOT EXISTS "FenixDomainAdministration"."domains"
 (
-    domain_uuid             uuid      not null
+    domain_uuid                     uuid      not null
         constraint domains_pk
             primary key,
-    domain_name             varchar   not null,
-    description             varchar,
-    activated               boolean   not null,
-    deleted                 boolean   not null,
-    update_timestamp        timestamp not null,
-    replaced_by_new_version boolean   not null,
-    domain_id               integer   not null,
-    domain_version          integer   not null
+    domain_name                     varchar   not null,
+    description                     varchar,
+    activated                       boolean   not null,
+    deleted                         boolean   not null,
+    update_timestamp                timestamp not null,
+    replaced_by_new_version         boolean   not null,
+    domain_id                       integer   not null,
+    domain_version                  integer   not null,
+    callingworkerserviceaccountname varchar   not null,
 );
 
-COMMENT ON TABLE "FenixDomainAdministration".domains is 'Domains that can call Fenix';
+comment on table "FenixDomainAdministration".domains is 'Domains that can call Fenix';
 
-ALTER TABLE "FenixDomainAdministration".domains
-    OWNER TO "postgres";
+alter table "FenixDomainAdministration".domains
+    owner to postgres;
 
 COMMIT ;
+
