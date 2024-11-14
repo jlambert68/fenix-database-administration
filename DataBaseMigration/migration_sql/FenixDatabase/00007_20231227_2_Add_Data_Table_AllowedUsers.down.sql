c98037eb9fcb7e696c4685ac13b1071d864534f5
jlambert68 <jonas.lambert@drakon.se>
2024-11-14 10:28:08 +0100

    Removed sql for adding allowed users

--- DataBaseMigration/migration_sql/FenixDatabase/00007_20231227_2_Add_Data_Table_AllowedUsers.down.sql
+++
@@ -1,8 +0,0 @@
--- noinspection SqlNoDataSourceInspectionForFile
-
-BEGIN;
-
--- Remove all data in Table "FenixDomainAdministration".domains
-DELETE FROM "FenixDomainAdministration"."allowedusers";
-
-COMMIT;
