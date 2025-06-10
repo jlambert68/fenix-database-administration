5a5e3ad3eecaf0665dcdb83de64d2a2f312b8c2e
jlambert68 <jonas.lambert@drakon.se>
2025-06-10 13:12:38 +0200

    Added table "TestSuites"

---
+++ DataBaseMigration/migration_sql/FenixDatabase/01031_20250610_1_Create_Table_TestSuites.up.sql
@@ -0,0 +1,38 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+BEGIN;
+
+create table "FenixBuilder"."TestSuites"
+(
+    "DomainUuid"                                                  uuid,
+    "DomainName"                                                  varchar,
+    "TestSuiteUuid"                                               uuid,
+    "TestSuiteName"                                               varchar,
+    "TestSuiteVersion"                                            integer,
+    "TestSuiteHash"                                               varchar,
+    "CanListAndViewTestSuiteAuthorizationLevelOwnedByDomain"      bigint,
+    "CanListAndViewTestSuiteAuthorizationLevelHavingTiAndTicWith" bigint,
+    "InsertTimeStamp"                                             timestamp,
+    "TestSuiteIsDeleted"                                          boolean,
+    "DeleteTimestamp"                                             timestamp default '2068-11-18 00:00:00'::timestamp without time zone,
+    "DeletedInsertedTImeStamp"                                    timestamp,
+    "DeletedByGCPAuthenticatedUser"                               varchar,
+    "TestCasesInTestSuite"                                        jsonb not null,
+    "TestSuitePreview"                                            jsonb not null,
+    "TestSuiteMetaData"                                           jsonb not null,
+    "UniqueCounter"                                               serial,
+    constraint testsuites_pk
+        unique ("TestSuiteUuid", "TestSuiteVersion", "TestSuiteIsDeleted")
+);
+
+comment on table "FenixBuilder"."TestSuites" is 'Holds all TestSuites as a JSONB-structure';
+
+alter table "FenixBuilder"."TestSuites"
+    owner to postgres;
+
+COMMIT ;
+
+
+
+
+
